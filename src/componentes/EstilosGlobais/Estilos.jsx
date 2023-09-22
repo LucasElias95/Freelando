eca9a8eefabfee52c5deb74c0b7968c65032f0dc
LucasElias95 <lucas.a.elias@gmail.com>
2023-09-22 12:38:01 -0300

    instalamos o emotion, adicionamos a font family padrao

---
+++ src/componentes/EstilosGlobais/Estilos.jsx
@@ -0,0 +1,10 @@
+import { Global, css } from "@emotion/react"
+
+const estilos = css`
+*{
+    font-family: 'Montserrat', sans-serif;
+}`
+
+export const Estilos = () =>{
+    return (<Global styles={estilos}/>)
+}
