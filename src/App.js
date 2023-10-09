c49b4d39bfad8fa218790da4dad023c628f66140
LucasElias95 <lucas.a.elias@gmail.com>
2023-10-09 11:51:23 -0300

    criando componentes Tipografia e CampoTexto

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { CampoTexto } from "./componentes/CampoTexto/CampoTexto.jsx";
 import { Card } from "./componentes/Card/card.jsx";
 import { Estilos } from "./componentes/EstilosGlobais/estilos.jsx";
 import { ProvedorTema } from "./componentes/ProvedorTema/ProvedorTema.jsx";
+import { Tipografia } from "./componentes/Tipografia/Tipografia";
 
-function App () {
-  return (
-      <ProvedorTema>
-          <Estilos />
-          <Card>
-              <h1>Freelando</h1>
-          </Card>
-      </ProvedorTema>
-  );
+function App() {
+    return (
+        <ProvedorTema>
+            <Estilos />
+            <Card>
+                <Tipografia varieante="h1" componente="h1">
+                    Freelando
+                </Tipografia>
+                <Tipografia variante="body" componente="body" >
+                Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers. Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho. 
+                </Tipografia>
+                <CampoTexto titulo="Nome Completo"/>
+            </Card>
+        </ProvedorTema>
+    );
 }
 
 export default App;
--- src/componentes/Card/card.jsx
+++ src/componentes/Card/card.jsx
@@ -1,5 +1,16 @@
 import styled from '@emotion/styled'
 
+
+export const Cabecalho = styled.header`
+    background: ${props => props.theme.cores.primarias.a};
+    padding: ${props => props.theme.espacamentos.m};
+`
+export const Rodape = styled.footer`
+background: ${props => props.theme.cores.primarias.a};
+padding: ${props => props.theme.espacamentos.xl};
+color: ${props => props.theme.cores.branco};
+`
+
 const DivEstilizada = styled.div`
     padding: ${props => props.theme.espacamentos.l};
     background: ${props => props.theme.cores.secundarias.a};
