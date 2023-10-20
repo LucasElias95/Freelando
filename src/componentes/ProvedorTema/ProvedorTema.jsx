9a0aa52c55c4594676050b963fa934c8b29c839c
LucasElias95 <lucas.a.elias@gmail.com>
2023-10-20 12:59:02 -0300

    adicionando react-grid-system + componente Botao

--- src/componentes/ProvedorTema/ProvedorTema.jsx
+++ src/componentes/ProvedorTema/ProvedorTema.jsx
@@ -1,37 +1,39 @@
 import { ThemeProvider } from '@emotion/react'
 
 const tema = {
-    cores:{
-        branco:``,
-        atencao:``,
-        focus:``,
-        primarias:{
-            a:`#5754ED`,
-            b:``,
-            c:``
+    cores: {
+        branco: '#FFFFFF',
+        atencao: '#FF0E0E',
+        focus: '#B009FF',
+        primarias: {
+            a: '#5754ED',
+            b: '#D93114',
+            c: '#168070'
         },
-        secundarias:{
-            a:`#EBEAF9`,
-            b:``,
-            c:``
+        secundarias: {
+            a: '#F8F8FD',
+            b: '#EBEAF9',
+            c: '#EBFCF9'
         },
-        neutras:{
-            a:`#373737`,
-            b:``,
-            c:``,
-            d:``
+        neutras: {
+            a: '#373737',
+            b: '#E8E8E8',
+            c: '#F5F5F5',
+            d: ''
         },
-        dark:{
-            a:``,
-            b:``
+        dark: {
+            a: '#110EA0',
+            b: '#D93114'
         }
     },
     espacamentos: {
-        xs: '9px',
-        s: '16px',
-        l: '32px',
-    }
+       xs: '8px',
+       s : '16px',
+       l : '32px',
+    },
+    fontFamily: "'Montserrat', sans-serif"
 }
+
 export const ProvedorTema = ({ children }) => {
     return <ThemeProvider theme={tema}>
         { children }
